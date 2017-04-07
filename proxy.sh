93b84dbfd783416e205425c1770cb44e6a191fcb
WZQ1397 <wzqsergeant@vip.qq.com>
2017-04-07 10:33:13 +0800

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,3 +1,6 @@
+#!/bin/bash  
+#        Author: Zach.Wang
+#        Create: 2017-04-07 10:32:59
 http_proxy="http://10.112.5.173:49908/"
 https_proxy="http://10.112.5.173:49908/"
 ftp_proxy="http://10.112.5.173:49908/"
