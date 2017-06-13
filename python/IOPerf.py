265be5467ced8ef0ee07ac87ad5fce31ca7f1921
WZQ1397 <wzqsergeant@vip.qq.com>
2017-06-13 16:07:10 +0800

    Create IOPerf.py

---
+++ python/IOPerf.py
@@ -0,0 +1,20 @@
+#!/bin/bash
+# author zach.wang
+diskname='/dev/sdb1'
+testname=`basename $diskname`
+taskname=$testname$type$blocksize
+arg1='-direct=1 -iodepth 1 -thread -rwmixread=70 -ioengine=psync -group_reporting'
+percent=4
+for blocksize in 4k 8k 16k 32k 4m 8m 16m 32m;
+do
+    for type in read write randrw;
+    do
+        echo
+        echo "===========" $taskname "============"
+        fio -filename=$diskname $arg1 -rw=$type -bs=$blocksize -size=20G -numjobs=30 -runtime=60 -name=$taskname >> $testname.log
+        echo "complete " $percent"%"
+#       let percent+=4
+        percent=`expr $percent + 4`
+    done
+done
+echo "complete " $percent"%"
