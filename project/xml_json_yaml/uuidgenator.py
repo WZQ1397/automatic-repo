06c858b4e220582a4b87f2ebfba7b1c74df25b6b
WZQ1397 <wzqsergeant@vip.qq.com>
2017-07-05 10:55:04 +0800

    uuid

---
+++ project/xml_json_yaml/uuidgenator.py
@@ -0,0 +1,32 @@
+import uuid
+
+u = uuid.uuid1()
+
+print(u)
+print(type(u))
+print('bytes   :', repr(u.bytes))
+print('hex     :', u.hex)
+print('int     :', u.int)
+print('urn     :', u.urn)
+print('variant :', u.variant)
+print('version :', u.version)
+print('  time_low            : ', u.time_low)
+print('  time_mid            : ', u.time_mid)
+print('  time_hi_version     : ', u.time_hi_version)
+print('  clock_seq_hi_variant: ', u.clock_seq_hi_variant)
+print('  clock_seq_low       : ', u.clock_seq_low)
+print('  node                : ', u.node)
+print('  time                : ', u.time)
+print('  clock_seq           : ', u.clock_seq)
+
+#TODO UUID 3 and 5 - Name-Based Values
+hostnames = ['www.1080pdy.com', 'zach.1080pdy.com']
+
+for name in hostnames:
+    print(name)
+    print('  MD5   :', uuid.uuid3(uuid.NAMESPACE_DNS, name))
+    print('  SHA-1 :', uuid.uuid5(uuid.NAMESPACE_DNS, name))
+
+#TODO UUID 4 - Random Values
+for i in range(3):
+    print(uuid.uuid4())
