a0a7df10c5acf6648f6ece97d219b37929690953
WZQ1397 <wzqsergeant@vip.qq.com>
2017-07-09 13:20:11 +0800

    Update wordsortNnum.sh

--- shell/wordsortNnum.sh
+++ shell/wordsortNnum.sh
@@ -17,4 +17,9 @@ fi
 
 tr -sc "[A-Z][a-z]"  "[\012*]"  < $I_FILE | tr  "[A-Z]"  "[a-z]"  | \  
 sort  | uniq -c | sort  -k1 -n -r  |  \  
-head -$I_TOP | nl  
+head -$I_TOP | nl
+
+
+#tr -cs "[a-z][A-Z][0-9]" "\n" |\ 
+#tr A-Z a-z | sort | uniq -c | sort -k1nr -k2 | head -n $count
+#tr是sed的简化，-c用前字符串中字符集的补集替换成后字符串即将不是字符和数字的单词替换换行
