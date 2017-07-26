5785e2534e61c01481feeb363c354646dff14c68
WZQ1397 <wzqsergeant@vip.qq.com>
2017-07-26 16:53:31 +0800

    env

---
+++ shell/openstack-script/env.sh
@@ -0,0 +1,24 @@
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=edong
+export OS_AUTH_URL=http://yz3ctrl1.dev3.tonyc.cn:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+
+
+export OS_PROJECT_DOMAIN_NAME=Default
+export OS_USER_DOMAIN_NAME=Default
+export OS_PROJECT_NAME=zach
+export OS_USERNAME=zach
+export OS_PASSWORD=edong
+export OS_AUTH_URL=http://yz3ctrl1.dev3.tonyc.cn:35357/v3
+export OS_IDENTITY_API_VERSION=3
+export OS_IMAGE_API_VERSION=2
+
+
+export OS_TOKEN=46cf6c7d62d8d13766c03acde4b336b8
+export OS_URL=http://yz3ctrl1.dev3.tonyc.cn:35357/v3
+export OS_IDENTITY_API_VERSION=3
+
---
+++ shell/openstack-script/key_init.sh
@@ -0,0 +1,7 @@
+keystone-manage fernet_setup --keystone-user keystone --keystone-group keystone
+keystone-manage credential_setup --keystone-user keystone --keystone-group keystone
+keystone-manage bootstrap --bootstrap-password ADMIN_PASS \
+  --bootstrap-admin-url http://controller:35357/v3/ \
+  --bootstrap-internal-url http://controller:35357/v3/ \
+  --bootstrap-public-url http://controller:5000/v3/ \
+  --bootstrap-region-id RegionOne
