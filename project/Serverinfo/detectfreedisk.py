d08eb63dd8884b624ceeba6e81638b9eedde8e19
WZQ1397 <wzqsergeant@vip.qq.com>
2017-08-01 11:52:41 +0800

    sys

---
+++ project/Serverinfo/detectfreedisk.py
@@ -0,0 +1,15 @@
+import ctypes
+import os
+import platform
+def get_free_space_mb(folder):
+    """ Return folder/drive free space (in bytes)
+    """
+    if platform.system() == 'Windows':
+        free_bytes = ctypes.c_ulonglong(0)
+        ctypes.windll.kernel32.GetDiskFreeSpaceExW(ctypes.c_wchar_p(folder), None, None, ctypes.pointer(free_bytes))
+        return free_bytes.value/1024/1024/1024
+    else:
+        st = os.statvfs(folder)
+        return st.f_bavail * st.f_frsize/1024/1024
+
+print(get_free_space_mb('C:\\'),'GB')
