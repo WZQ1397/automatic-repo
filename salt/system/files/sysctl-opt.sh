3d820e6d021558ba43df906bc0d7a9ae6dac3d04
WZQ1397 <wzqsergeant@vip.qq.com>
2017-08-06 20:04:22 +0800

    Create sysctl-opt.sh

---
+++ salt/system/files/sysctl-opt.sh
@@ -0,0 +1,26 @@
+net.ipv4.tcp_fin_timeout=2
+net.ipv4.tcp_tw_reuse=1
+net.ipv4.tcp_tw_recycle=1
+net.ipv4.tcp_syncookiies=1
+net.ipv4.tcp_keepalive_time=600
+net.ipv4.ip_local_port_range=4000 65000
+net.ipv4.tcp_max_syn_backlog=16384
+net.ipv4.tcp_max_tw_buckets=36000
+net.ipv4.route.gc_timeout=100
+net.ipv4.tcp_syn_retries=1
+net.ipv4.tcp_synack_retries=1
+net.core.somaxconn=16384
+net.core.netdev_max_backlog=16384
+net.ipv4.tcp_max_orphans=16384
+net.nf_conntrack_max=25000000
+net.netfilter.nf_conntrack_max=25000000
+net.netfilter.nf_conntrack_tcp_timeout_established=180
+net.netfilter.nf_conntrack_tcp_timeout_time_wait=120
+net.netfilter.nf_conntrack_tcp_timeout_close_wait=60
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait=120
+
+net.netfilter.nf_conntrack_max = 25000000
+net.netfilter.nf_conntrack_tcp_timeout_established = 180
+net.netfilter.nf_conntrack_tcp_timeout_time_wait = 120
+net.netfilter.nf_conntrack_tcp_timeout_close_wait = 60
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 120
