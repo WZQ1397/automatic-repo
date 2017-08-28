820424fa9b70b12881e786473e4e9d54d031fb13
WZQ1397 <wzqsergeant@vip.qq.com>
2017-08-28 09:19:04 +0800

    network

---
+++ shell/network/check_url.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#        Author: Zach.Wang
+#        Create: 2017-04-13 20:40:37
+# how to
+# sh check_url.sh filename
+[ -f /etc/init.d/functions ] && source /etc/init.d/functions
+#check_url
+url_file=$1
+wait(){
+echo -n "wait 3s"
+for((i=0;i<3;i++))
+do
+  echo -n "."
+  sleep 1
+done
+echo 
+}
+check_url(){
+  wget -T 5 -t 2 --spider $1 &>/dev/null
+  RETVAL=$?
+  if [ $RETVAL -eq 0 ];then
+      action "check $1"  /bin/true
+  else
+      action "check $1"  /bin/false   
+  fi
+  return $RETVAL
+}
+main(){
+  wait
+  while read line
+  do
+    check_url $line
+  done < $url_file
+}
+main
---
+++ shell/network/count_feqreq.sh
@@ -0,0 +1,22 @@
+﻿#!/bin/bash 
+# 
+# Filename:  count_req.sh 
+# Description: 统计Nginx日志里IP访问频率 
+ 
+NGINXLOG="./access.log"
+start_time=$(head -n1 "$NGINXLOG" | grep -o " \[.*\] ") 
+stop_time=$(tail -n1 "$NGINXLOG" | grep -o " \[.*\] ") 
+echo -e "start:\t\e[92m$start_time\033[0m"
+echo -e "stop:\t\e[92m$stop_time\033[0m"
+echo '所有的请求TOP50-->>'
+# 所有的请求 
+cat "$NGINXLOG" | awk '{++S[$1]} END {for(a in S) print S[a],"\t", a}' | sort -rn -k1 | head -n 50 
+echo '--------------------------------------------------'
+echo '成功的请求TOP50-->>'
+# 成功的请求 
+grep ' 200 ' "$NGINXLOG" | awk '{++S[$1]} END {for(a in S) print S[a],"\t", a}' | sort -rn -k1 | head -n 50 
+
+#for ip in `grep ' 200 ' "$NGINXLOG" | awk '{++S[$1]} END {for(a in S) print S[a],"\t", a}' | sort -rn -k1 | head -n 10`
+#do
+#iptables -I INPUT -s ${ip} -j DROP
+#done
