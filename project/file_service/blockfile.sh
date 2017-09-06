3e0158a5633384dac8ccc5304d7ab9f2df31dc91
WZQ1397 <wzqsergeant@vip.qq.com>
2017-09-06 20:34:49 +0800

    backup

---
+++ project/file_service/blockfile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash  
+#        Author: Zach.Wang
+#        Create: 2017-04-06 10:15:59
+#HOW TO 
+#./createfile.sh aaa 10 50 bin /aaa
+NAME=$1
+MYPATH=$5
+START=$2
+END=$3
+TAIL=$4
+
+
+[ -z $MYPATH ] && MYPATH=/zach_dir/
+
+[ -d $MYPATH ] || mkdir $MYPATH
+
+
+for (( i = $START ; i <= $END;i++ ))
+do
+     PERFIX=`< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c12`
+     echo $MYPATH/${PERFIX}_$NAME-$i.$TAIL
+     touch $MYPATH/${PERFIX}_$NAME-$i.$TAIL
+done
+
