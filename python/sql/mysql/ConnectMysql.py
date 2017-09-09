7063150ca82f9ca0191dfdb7d3df1cb0f578fa04
WZQ1397 <wzqsergeant@vip.qq.com>
2017-09-09 13:23:13 +0800

    mysql

---
+++ python/sql/mysql/ConnectMysql.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# encoding: utf-8
+# -*- coding: utf8 -*-
+"""
+pip install MySQL-python
+"""
+
+import MySQLdb
+import datetime
+import sys
+
+try:
+    sql = 'select now()'
+    conn = MySQLdb.Connect(host='localhost', user='root', passwd='', db='test', port=3306, charset='utf8')
+    cur = conn.cursor()
+    start_time = datetime.datetime.now()
+    rows = cur.execute(sql)
+    result = cur.fetchall()
+    end_time = datetime.datetime.now()
+    time = (end_time - start_time).microseconds / 1000000.000
+    print "%d rows in set (%f sec)" % (rows, time)
+    for record in result:
+        print record
+    cur.close()
+    conn.close()
+except MySQLdb.Error, e:
+    print "Mysql Error %d: %s" % (e.args[0], e.args[1])
+    sys.exit(1)
