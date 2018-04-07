705cb200c6413db8af57ebbce29e89ef8f1c5d4b
WZQ1397 <wzqsergeant@vip.qq.com>
2018-04-07 16:42:30 +0800

    Update decorate_demo.py

--- python/decorate_demo.py
+++ python/decorate_demo.py
@@ -1,6 +1,6 @@
 # author zach.wang
 # -*- coding:utf-8 -*-
-"""def help(ch):
+def help(ch):
     def basic(choice):
         if choice == "a":
             basicuse()
@@ -24,7 +24,7 @@ def basicuse():
 basicuse = basicuse2(basicuse)
 basicuse()
 
-"""
+
 def help2(fun):
     def basicuse2():
         fun()
@@ -42,7 +42,7 @@ basicuse()
 def help3(fun):
     def basicuse2():
         fun()
-        print("bbb")
+        print("bbb\n++++")
     return basicuse2
 
 @help3
@@ -50,3 +50,19 @@ def basicuse():
     print("aaa")
 
 basicuse()
+
+#--------------
+
+def help5(fun):
+    def basicuse2(*args,**kwargs):
+        print("before\n")
+        res = fun(*args,**kwargs)
+        print("bbb\n",res)
+    return basicuse2
+
+@help5   #help5 = basicuse(help5)
+def basicuse(x,y,z):
+    print("aaa",x,y,z)
+    return "ok"
+
+basicuse("wzq","lts",[chr(_) for _ in range(ord('a'), ord('z')+1)])
