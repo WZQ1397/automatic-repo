53a293070263bb866566d7e1032481442ceefd81
WZQ1397 <wzqsergeant@vip.qq.com>
2018-05-18 19:49:37 +0800

    Create MultiprocessingPool.py

---
+++ python/process_thread/MultiprocessingPool.py
@@ -0,0 +1,37 @@
+from multiprocessing import Process,Pool
+import time,os
+
+content = [chr(ord('A')+x) for x in range(0,10)]
+
+def fun_p(x):
+    time.sleep(1)
+    print(x,__name__,os.getpid())
+    #TODO return value to callback
+    return x
+
+def end(x):
+    """
+    function end is a parent pid
+    :param x: fun_p(pid) return to ppid
+    """
+    str_line = "-"*10
+    print("{3}--{2}:{0} {1} {0}".format(str_line,time.ctime(),os.getpid(),x))
+
+if __name__ == '__main__':
+    pool = Pool(5)
+    count = 0
+    method = input("choose method SYNC or ASYNC\n:>")
+    for x in content:
+        if method == "S".lower():
+            #A--B--C--D
+            pool.apply(fun_p,args=(x,))
+        if method == "A".lower():
+            #A--B-->
+            #   C-->
+            #   D-->
+            pool.apply_async(fun_p, args=(x,),callback=end)
+        count = count + 1
+
+    print(count,__name__,os.getpid())
+    pool.close()
+    pool.join()
