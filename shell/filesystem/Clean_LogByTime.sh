2068a17cb68efbfe9d0fa11de456aeba14c90230
WZQ1397 <wzqsergeant@vip.qq.com>
2018-07-09 16:12:04 +0800

    LOG

---
+++ shell/filesystem/Clean_LogByTime.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Usage Clean_Log.sh PATH TIME
+DAYS=$2
+CLEAN_PATH=$1
+
+cd $CLEAN_PATH
+if [[ `pwd` == $CLEAN_PATH && $DAYS -ge 3 ]];
+then
+  find -ctime +$DAYS | xargs rm -rf \{};
+  echo $PATH "CLEAN SUCCESS!" date >> /tmp/zach_clean.log
+else
+  echo $PATH "CLEAN failed!" date >> /tmp/zach_clean.log
+fi
+echo "+++++++++++++++++++++++" >> /tmp/zach_clean.log
+sleep 30
+> catalina.out
