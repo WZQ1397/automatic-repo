86678a65d091ec4787bd5247196136c79f61746d
WZQ1397 <wzqsergeant@vip.qq.com>
2018-08-28 09:57:36 +0800

    log_clean

--- shell/filesystem/Clean_LogByTime.sh
+++ shell/filesystem/Clean_LogByTime.sh
@@ -7,9 +7,9 @@ cd $CLEAN_PATH
 if [[ `pwd` == $CLEAN_PATH && $DAYS -ge 3 ]];
 then
   find -ctime +$DAYS | xargs rm -rf \{};
-  echo $PATH "CLEAN SUCCESS!" date >> /tmp/zach_clean.log
+  echo $CLEAN_PATH "CLEAN SUCCESS!" date >> /tmp/zach_clean.log
 else
-  echo $PATH "CLEAN failed!" date >> /tmp/zach_clean.log
+  echo $CLEAN_PATH "CLEAN failed!" date >> /tmp/zach_clean.log
 fi
 echo "+++++++++++++++++++++++" >> /tmp/zach_clean.log
 sleep 30
