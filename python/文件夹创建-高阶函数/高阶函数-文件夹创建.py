6ab8d65d35af8c30bbe616ef3f095f7d500a8624
WZQ1397 <wzqsergeant@vip.qq.com>
2019-09-05 09:45:45 +0800

    Create 高阶函数-文件夹创建.py

---
+++ python/文件夹创建-高阶函数/高阶函数-文件夹创建.py
@@ -0,0 +1,88 @@
+# author zach.wang
+# -*- coding:utf-8 -*-
+import sys,getopt
+from DirCreateMethod import BatchDirDeploy,CreateDir
+arglst=""
+
+def bysuffix(name=""):
+    '''
+    CREATE FILE NAME LIKE :  Xxx-1,Xxx-3 if you input the suffix name
+    :param name: default -> none
+    :return:
+    '''
+    return fun.suffix(name)
+
+def byprefix(name=""):
+    '''
+    CREATE FILE NAME LIKE :  1-xxx,3-xxx if you input the prefix name
+    :param name: default -> Zach
+    :return:
+    '''
+    return fun.prefix(name)
+
+def bydate(spec="today",count=0):
+    '''
+    CREATE FILE NAME LIKE :  1-xxx,3-xxx if you input the prefix name
+    :param spec: default -> today FORMAT : 2019-09-04
+    :return:
+    '''
+    if spec!="today" and not count:
+        return fun.usedatetime(spec)
+    else:
+        return fun.usedatetime()
+
+def full(spec,args):
+    '''
+    CREATE FILE NAME LIKE :  1-xxx,3-xxx if you input the prefix name
+    :param spec: default -> today FORMAT : 2019-09-04
+    :return:
+    '''
+    # print(args)
+    byprefix(args[0])
+    if len(args)>=2:
+        bysuffix(args[1])
+    else:
+        bysuffix()
+    # print("1", spec)
+    return bydate(spec,len(args))
+
+def deploy(func):
+    '''
+    A main 高阶函数调用入口函数
+    :param func:
+    :return:
+    '''
+    print("loading ...")
+    # judge the num of args to execute
+    Exec = func(funcarg,arglst) if len(arglst)>=1 else func(funcarg) if func.__name__!="full" else "ARGS ERROR!"
+    return Exec
+
+try:
+   opts, args = getopt.getopt(sys.argv[1:],"hn:p:s:d:a",["prefix=","suffix=","date="])
+except getopt.GetoptError:
+   sys.exit(2)
+for opt, arg in opts:
+   if opt == '-n':
+      num=arg
+   elif opt in ("-p", "--prefix"):
+      func ,funcarg = byprefix, arg
+   elif opt in ("-s", "--suffix"):
+      func ,funcarg = bysuffix, arg
+   elif opt in ("-d", "--date"):
+      func ,funcarg = bydate, arg
+      # print(arg)
+   elif opt in ("-a", "--all"):
+      func ,funcarg = full, arg
+      # print(arg)
+      arglst=sys.argv[5:]
+   else:
+       print(opt,arg)
+
+fun = BatchDirDeploy(eval(num))
+
+# 使用eval()实现字符串转函数对象
+# print(deploy(eval(sys.argv[2])))
+deploy=CreateDir(deploy(func))
+deploy.Create()
+
+
