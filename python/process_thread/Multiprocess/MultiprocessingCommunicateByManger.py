1dc5e4974046853dad90d3d2a8a0038b53518e35
WZQ1397 <wzqsergeant@vip.qq.com>
2020-02-09 21:50:30 +0800

    多进程

---
+++ python/process_thread/Multiprocess/MultiprocessingCommunicateByManger.py
@@ -0,0 +1,32 @@
+from multiprocessing import Process,Manager
+import random
+
+# PTA 重排链表
+def exchange(L1, L2, L3):
+  L2.reverse()
+  x ,y = L1, L2
+  z = []
+  while L1 and L2:
+    L3.append(L2[0])
+    L3.append(L1[0])
+    L1.pop(0)
+    L2.pop(0)
+
+  while L1:
+    L3.append(L1[0])
+    L1.pop(0)
+  while L2:
+    L3.append(L2[0])
+    L2.pop(0)
+
+
+if __name__ == '__main__':
+  with Manager() as manager:
+    num1 = input("list1:")
+    num2 = input("list2:")
+    sortedlist = manager.list()
+    p = Process(target=exchange, args=(list(num1), list(num2), sortedlist))
+    p.start()
+    p.join()
+    print("".join(sortedlist))
+    p.terminate()
---
+++ python/process_thread/Multiprocess/MultiprocessingCommunicateByPipe.py
@@ -0,0 +1,26 @@
+from multiprocessing import Process, Pipe
+import os, time, random
+
+
+# 多进程通信Pipe
+def write(p):
+  for value in ['A', 'B', 'C']:
+    print('Put %s to queue...' % value)
+    p.send(value)
+    time.sleep(random.random())
+
+
+def read(p):
+  while True:
+    value = p.recv()
+    print('Get %s from queue.' % value)
+
+
+if __name__ == '__main__':
+  parent , child = Pipe()
+  pw = Process(target=write, args=(parent,))
+  pr = Process(target=read, args=(child,))
+  pw.start()
+  pr.start()
+  pw.join()
+  pr.terminate()
