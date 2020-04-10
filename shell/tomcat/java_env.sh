759a35d484a5a3b275b5f4e8e1d4fb94f3118d7b
WZQ1397 <wzqsergeant@vip.qq.com>
2020-04-10 15:53:10 +0800

    Update java_env.sh

--- shell/tomcat/java_env.sh
+++ shell/tomcat/java_env.sh
@@ -5,3 +5,5 @@ export MAVEN_HOME
 export JAVA_HOME
 export JRE_HOME
 export PATH=${PATH}:${MAVEN_HOME}/bin:${JAVA_HOME}:${JRE_HOME}
+export JAVA_OPTS="$JAVA_OPTS -server -Xmn512m -Xms512m -Xmx1024m -XX:MaxNewSize=512m -XX:PermSize=512m -XX:MaxPermSize=512m -verbose:gc -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintTenuringDistribution -XX:+PrintHeapAtGC -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+
+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/data/web -Xloggc:/data/web/collectionweb_gc.log"
