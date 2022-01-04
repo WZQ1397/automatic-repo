4c8e590a382c4d53c89b1dca81017042e2e582a7
WZQ1397 <wzqsergeant@vip.qq.com>
2022-01-04 14:40:00 +0800

    Create getSN.sh

---
+++ shell/system/getSN.sh
@@ -0,0 +1,16 @@
+#Author: zach.wang
+# apt install -y dmidecode lshw smartmontools
+randomfile=`cat /dev/urandom | od -x  | head -c 48 | sed -e "s/0\{5,\}//g" -e "s/ //g"`
+
+echo "BIOS"
+dmidecode -t 2 | grep Serial | awk '{print $NF}' | tee -a $randomfile
+echo "NIC"
+lshw -c network | grep serial | head -n 1 | awk '{print $NF}' | tee -a $randomfile
+echo "CPU"
+dmidecode -t 4 | grep ID | sed -e "s/ID://g" -e "s/\t//g"| uniq | tee -a $randomfile
+echo "OEM uuid"
+dmidecode –t 11 | grep -i uuid |  awk '{print $NF}' | tee -a $randomfile
+echo "DISK SN"
+for disk in /dev/sd[a-z]; do echo "DISK $disk SN:"; smartctl -i $disk | grep "Serial Number" | awk '{print $NF}' | tee -a $randomfile; done
+echo "======================== hardcode ========================="
+sha512sum  $randomfile | cut -d ' ' -f 1 | tee hdcode.txt
