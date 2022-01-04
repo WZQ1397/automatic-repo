8b4a380766e15a0740e2178944d33d4b2e56dcc1
WZQ1397 <wzqsergeant@vip.qq.com>
2022-01-04 15:33:07 +0800

    Create syncdata-byParameter.sh

---
+++ shell/filesystem/syncdata-byParameter.sh
@@ -0,0 +1,18 @@
+src=$1
+dst=$2
+targetsvc="6.6.6.6:18008"
+if [[ $# -ge 2 ]] && [[ $# -le 3 ]];
+then
+  if [[ $# -eq 3 ]];
+  then
+    targetsvc=$3
+  fi
+  echo "starting ..."
+else
+  echo "Parameter not enough, EG: $0 7514 /data/imgs /data/ocr/999-jklaaa"
+  exit -1
+fi
+port=`echo $targetsvc | cut -d ':' -f 2`
+addr=`echo $targetsvc | cut -d ':' -f 1`
+echo "send to server $addr, port $port"
+rsync -avzP -e "ssh -p $port" $src dev@$addr:/$dst
