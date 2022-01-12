6fd173daf8644474f63b94abcb48b59a920d311d
WZQ1397 <wzqsergeant@vip.qq.com>
2022-01-12 09:53:16 +0800

    Update check_vaild_file.sh

--- shell/filesystem/check_vaild_file.sh
+++ shell/filesystem/check_vaild_file.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function check_size(){
-  meta_info_size=`curl -s -I 6.6.6.6:8081/job/app-sit/1342/execution/node/3/ws/target/app-0.0.1-SNAPSHOT.jar | grep Content-Length | cut -d ' ' -f 2 | sed 's/\r//g'` 
-  current_size=`du -b /tsjr-data/deploy-java/tsjr-app-sit-8080.jar | awk '{print $1}'`
+  meta_info_size=`curl -s -I 43.254.152.164:8081/job/suncash-approval-sit/1342/execution/node/3/ws/target/approval-0.0.1-SNAPSHOT.jar -u devops:tsjinrong | grep Content-Length | cut -d ' ' -f 2 | sed 's/\r//g'` 
+  current_size=`du -b /tsjr-data/deploy-java/tsjr-suncash-approval-sit-8080.jar | awk '{print $1}'`
   echo "ori:" $meta_info_size
   echo "current:" $current_size
   if [[ $meta_info_size -eq $current_size ]]
